4b9698427c1ab626506527de5839c0cd32590b08
NafijurRN <nafijurrna@gmail.com>
2025-02-15 01:12:04 +0600

    Add files via upload

---
+++ box.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int t;
+cin >> t;
+
+while(t--){
+int m;
+cin >> m;
+for(int i=0; i<m ; i++){
+    for(int j= 0; j<m; j++){
+
+        cout<<"*";
+    }
+cout <<endl;
+
+}
+if (t){
+    cout<<endl;
+}
+}
+return 0;
+
+
+
+}
