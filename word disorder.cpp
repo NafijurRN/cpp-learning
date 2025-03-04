33ceb7d35fa27b97ea340909ef307cc1f8ade35f
Md Nafijur Rahman Nasrat <nafijurrna@gmail.com>
2025-03-04 19:05:25 +0600

    Add files via upload

---
+++ word disorder.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>  // reverse functrion er jonno
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();  //  remove '\n'
+
+    while (t--) {
+        string line;
+        getline(cin, line);  // next line input
+
+        stringstream ss(line);  // stringstream use for find out line's word
+        string word;
+
+        while (ss >> word) {
+            reverse(word.begin(), word.end()); //reverse the word
+            cout << word << " ";  // print the reverse word
+        }
+
+    cout << endl;  // পরবর্তী টেস্ট কেসের জন্য নতুন লাইন
+    }
+
+    return 0;
+}
